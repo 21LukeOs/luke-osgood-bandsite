75f9f4500c5f6e74ba8bcf7caf479a09b5687d8b
Luke Osgood <lukemosgood@gmail.com>
2019-10-21 20:30:28 -0400

    final

--- sprint-2/scripts/script.js
+++ sprint-2/scripts/script.js
@@ -1,11 +1,58 @@
 
 
-function getComment() {
+let comments = [
+  {
+    name: 'Michael Lyons',
+    comment: 'They BLEW the ROOF off at their last show'
+  }
+];
 
+let commentForm = document.querySelector('.join-conv__comments');
+
+function submit(e) {
+  e.preventDefault();
+  let commentorName = document.querySelector(".join-conv__input1").value;
+  let commentorComment = document.querySelector(".join-conv__input2").value;
+
+  comments.push({
+    name: commentorName,
+    comment: commentorComment
+  });
+
+  document.querySelector(".join-conv__input1").value = "";
+
+  document.querySelector(".join-conv__input2").value = "";
+
+  let newComment = {
+    name: commentorName,
+    comment: commentorComment
+  }
+  displayComments(newComment);
 }
 
-function addComment() {
+console.log(comments);
 
-}
+commentForm.addEventListener('submit', submit);
 
-function 
+function displayComments(comment) {
+
+  
+  document.querySelector(".join-conv__comment-list").innerHTML = "";
+  comments.forEach((comment) =>{
+    let comment1 = document.createElement('li');
+    comment1.innerHTML = `
+    <li class="join-conv__comment-list-item">
+    <div class="join-conv__comment-img">
+      <img src="" alt="">
+    </div>
+    <div class="join-conv__comment-name-text">
+      <div class="join-conv__comment-name-date">
+        <h4 class="join-conv__comment-name">${comment.name}</h4>
+        <h4 class="join-conv__comment-date">12/18/2018</h4>
+      </div>
+      <p class="join-conv__comment-text">${comment.comment}</p>
+    </div>
+    `
+    document.querySelector(".join-conv__comment-list").appendChild(comment1);
+  });
+};
